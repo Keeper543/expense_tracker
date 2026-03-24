1603a120c32f905d13a5246d64b8ef355a1194fc
tammiamckenzie <mckenzietammia@gmail.com>
2026-03-24 12:20:45 -0400

    started the intial the framwork

---
+++ lib/expenses.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget{
+  const Expenses({super.key});
+  
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ExpensesState();
+  }
+
+  
+  
+  }
+
+  class _ExpensesState extends State<Expenses>{
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("the chart goes here"),
+          SizedBox(height: 30,),
+          Text("EXPENSES LIST GOES HERE")
+        ],
+      ),
+    );
+  }
+
+  }
+
