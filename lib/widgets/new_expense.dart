8389fd2b799fa1e70b08b05d06aaf61a097ab9ec
tammiamckenzie <mckenzietammia@gmail.com>
2026-04-07 02:42:52 -0400

    added amount controller

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -9,6 +9,7 @@ class NewExpense extends StatefulWidget {
 }
 class _NewExpenseState extends State<NewExpense>{
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Padding(padding: EdgeInsets.all(16),
@@ -19,15 +20,24 @@ class _NewExpenseState extends State<NewExpense>{
           maxLength: 50,
           keyboardType: TextInputType.name,
           decoration: InputDecoration(
-          label: Text("title")
+          label: Text("Title")
           ),
         ),
-        Row(children: [
-
-        ],),
+      
+          TextField(
+          controller: _amountController,
+          maxLength: 10,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            prefixText: '\$',
+          label: Text("Amount"),
+          ),
+        ),
+       
         Row(children: [
             ElevatedButton(onPressed: (){
             print(_titleController.text);
+             print(_amountController.text);
             }, 
             child: Text('Save Expense'))
 
