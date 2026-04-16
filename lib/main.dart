36e75651ae898dd3a79eb3bfaa9d59f227a384b7
tammiamckenzie <mckenzietammia@gmail.com>
2026-04-16 12:07:46 -0400

    added bucket (is broken)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor:
   const Color.fromARGB(255, 96, 59,181),
@@ -7,7 +8,8 @@ var kColorScheme = ColorScheme.fromSeed(seedColor:
 var kDarkColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59,181)
 );
 void main() {
-  runApp( MaterialApp(
+  runApp( 
+    MaterialApp(
     theme: ThemeData(
       textTheme: ThemeData().textTheme.copyWith(
         titleLarge: TextStyle(
